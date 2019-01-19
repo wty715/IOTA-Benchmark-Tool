fbfb65c3ef505790fd54c83a5ae1767b271866b1
wty715 <wty715@163.com>
2019-01-19 16:44:05 +0800

    IOTA IRI (Full Node)
    IRI implementation

---
+++ IOTA/api-getNodeInfo.sh
@@ -0,0 +1,5 @@
+curl http://10.0.0.61:14265 \
+  -X POST \
+  -H 'Content-Type: application/json' \
+  -H 'X-IOTA-API-Version: 1' \
+  -d '{"command": "getNodeInfo"}'
---
+++ IOTA/start.sh
@@ -0,0 +1,1 @@
+nohup java -jar iri-1.5.5.jar -c iota.ini >> iota_output.log 2>&1 &
