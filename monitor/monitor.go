64123a1fa4a1d953badad292a8eeea8b8b16ead3
wty715 <wty715@163.com>
2019-02-18 10:50:48 +0800

    Add files via upload

--- monitor/monitor.go
+++ monitor/monitor.go
@@ -7,12 +7,19 @@ import (
 	"time"
 	"fmt"
 	"auto-wallet/monitor/transactions"
+	"github.com/pebbe/zmq4"
+	"encoding/json"
+	"io/ioutil"
 )
 
 func Shutdown(timeout time.Duration) {
 	select {
 	case <-time.After(timeout):
 	}
+}
+
+type Config struct {
+    Addr string
 }
 
 func main() {
@@ -23,10 +30,21 @@ func main() {
 	major, minor, patch := zmq4.Version()
 	fmt.Printf("running ZMQ %d.%d.%d\n", major, minor, patch)
 
+	// read config file
+	value := Config{}
+	fileBytes, err := ioutil.ReadFile(fileLocation)
+	if err != nil {
+		panic(err)
+	}
+	if err := json.Unmarshal(fileBytes, value); err != nil {
+		panic(err)
+	}
+	fmt.Printf("ZMQ addr: %s\n", value.Addr)
+
 	// start feeds
-	go transactions.startTxFeed()
-	go transactions.startMilestoneFeed()
-	go transactions.startConfirmationFeed()
+	go transactions.startTxFeed(value.Addr)
+	go transactions.startMilestoneFeed(value.Addr)
+	go transactions.startConfirmationFeed(value.Addr)
 
 	select {
 	case <-sigs:
