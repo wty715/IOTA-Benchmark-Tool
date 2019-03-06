5ac0ff8654dc6e393b39a721d81bd61ecdf6b158
wty715 <wty715@163.com>
2019-03-06 18:01:45 +0800

    BFS finished

--- monitor/transactions/transactions.go
+++ monitor/transactions/transactions.go
@@ -236,9 +236,11 @@ func StartDoubleFeed(address string) {
         for que.Len() != 0 {
             cur := que.Front()
             que.Remove(cur)
+            doubles[cur].visited = true
             effected_txs++
-            for _, v := range double_effects[cur].TXs {
-                que.PushBack(v.Hash)
+            for _, v := range doubles[cur].TXs {
+                if !doubles[v.Hash].visited
+                    que.PushBack(v.Hash)
             }
         }
         fmt.Printf("double-spending effected %d txs to be reattached.\n", effected_txs)
